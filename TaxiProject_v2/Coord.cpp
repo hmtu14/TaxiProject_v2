bd557cb4b463535a919a53a2231905d39c608778
gofortargets <ntanh14@apcs.vn>
2017-05-12 14:34:22 +0700

    add distance

--- TaxiProject_v2/Coord.cpp
+++ TaxiProject_v2/Coord.cpp
@@ -1,6 +1,31 @@
 #include "Coord.h"
 #include "cmath"
 
+
+#define earthRadiusKm 6371.0
+#define M_PI 3.14159265358979323846
+
+// This function converts decimal degrees to radians
+double deg2rad(double deg) {
+	return (deg * M_PI / 180);
+}
+
+//  This function converts radians to decimal degrees
+double rad2deg(double rad) {
+	return (rad * 180 / M_PI);
+}
+
+double distanceEarth(double lat1d, double lon1d, double lat2d, double lon2d) {
+	double lat1r, lon1r, lat2r, lon2r, u, v;
+	lat1r = deg2rad(lat1d);
+	lon1r = deg2rad(lon1d);
+	lat2r = deg2rad(lat2d);
+	lon2r = deg2rad(lon2d);
+	u = sin((lat2r - lat1r) / 2);
+	v = sin((lon2r - lon1r) / 2);
+	return 2.0 * earthRadiusKm * asin(sqrt(u * u + cos(lat1r) * cos(lat2r) * v * v));
+}
+
 Coord::Coord(double lat__, double lng__) {
 	lat = lat__;
 	lng = lng__;
@@ -18,9 +43,7 @@ double Sqr(double x)
 
 double Coord::Distance(Coord nCoord)
 {
-	double v1 = Sqr(lat - nCoord.lng); 
-	double v2 = Sqr(lng - nCoord.lat);
-	return sqrt(v1 + v2); 
+	return distanceEarth(this->lat, this->lng, nCoord.lat, nCoord.lng); 
 }
 
 Coord::~Coord()
