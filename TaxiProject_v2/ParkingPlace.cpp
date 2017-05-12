1cddd4f0c5eb612bfba95ab811f50495362a72e4
gofortargets <ntanh14@apcs.vn>
2017-05-12 16:22:20 +0700

    bool ParkingPlace::inParking(Coord x)

--- TaxiProject_v2/ParkingPlace.cpp
+++ TaxiProject_v2/ParkingPlace.cpp
@@ -1,5 +1,6 @@
 #include "ParkingPlace.h"
 
+
 ParkingPlace::ParkingPlace(vector<Coord> lstCoord__) {
 	lstCoord = lstCoord__;
 	double xmin = lstCoord__[0].lat;
@@ -20,9 +21,58 @@ ParkingPlace::ParkingPlace()
 {
 }
 
-bool ParkingPlace::inParking(Coord co__)
+Coord TopLeftCoord(vector<Coord> S)
 {
-	return false;
+	double lat_min = S[0].lat; 
+	for (int i = 0; i < S.size(); i++)
+		lat_min = min(lat_min, S[i].lat); 
+
+	double lng_min = 0; 
+	for (int i = 0; i < S.size(); i++)
+		if (S[i].lat == lat_min)
+		{
+			if (lng_min == 0) lng_min = S[i].lng;
+			else
+				if (S[i].lng < lng_min)
+					lng_min = S[i].lng;
+		}
+	
+	return Coord(lat_min, lng_min); 
+}
+
+
+Coord BottomRightCoord(vector<Coord> S)
+{
+	double lat_max = S[0].lat;
+	for (int i = 0; i < S.size(); i++)
+		lat_max = max(lat_max, S[i].lat);
+
+	double lng_max = 0;
+	for (int i = 0; i < S.size(); i++)
+		if (S[i].lat == lat_max)
+		{
+			if (lng_max == 0) lng_max = S[i].lng;
+			else
+				if (S[i].lng > lng_max)
+					lng_max = S[i].lng;
+		}
+
+	return Coord(lat_max, lng_max);
+}
+
+bool ParkingPlace::inParking(Coord x)
+{
+	if (x.lat < this->topleft.lat || x.lng < this->topleft.lng)
+		return false; 
+	if (x.lat > this->btmright.lat || x.lng > this->btmright.lng)
+		return false;
+	Coord TopLeft = TopLeftCoord(this->lstCoord);
+	Coord BottomRight = BottomRightCoord(this->lstCoord); 
+	if (x.lat < TopLeft.lat || x.lng < TopLeft.lng)
+		return false;
+	if (x.lat > BottomRight.lat || x.lng > BottomRight.lng)
+		return false;
+	return true; 
 }
 
 
--- TaxiProject_v2/ParkingPlace.h
+++ TaxiProject_v2/ParkingPlace.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include "Coord.h"
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
 class ParkingPlace
@@ -14,6 +16,8 @@ public:
 	ParkingPlace();
 	//kiem tra 1 coord co trong ParkingPlace k
 	bool inParking(Coord co__);
+	//friend Coord BottomRightCoord(vector<Coord>);
+	//friend Coord TopLeftCoord(vector<Coord>);
 
 	~ParkingPlace();
 };
