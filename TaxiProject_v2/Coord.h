5daddb9ed565164a8ce656d59b27854686fdbb19
minhtu <hoangminhtu@vietbando.com>
2017-05-19 10:25:04 +0700

    Edit struct

--- TaxiProject_v2/Coord.h
+++ TaxiProject_v2/Coord.h
@@ -1,12 +1,15 @@
 #pragma once
-class Coord {
-	
-public:
-	double lat;
-	double lng;
-	Coord();
-	Coord(double lat__, double lng__);
-	//Tinh khoang cach
-	double Distance(const Coord &nCoord);
-	~Coord();
+#include <cmath>
+struct Coord
+{
+	double	Latitude;
+	double Longitude;
+	Coord() {};
+	Coord(double lat, double lng) :Latitude(lat), Longitude(lng) {};
+	double realDistance(Coord coor_) {
+		double dlong = abs(this->Latitude - coor_.Latitude);
+		double cosD = (sin(this->Longitude)*sin(coor_.Longitude) + cos(this->Longitude)*cos(coor_.Longitude)*cos(dlong));
+		double D = acos(cosD);
+		return 1.852*D;
+	}
 };
--- TaxiProject_v2/datetime.h
+++ TaxiProject_v2/datetime.h
@@ -1,11 +1,60 @@
 #pragma once
-
-class DateTime
+struct DateTime
 {
-public:
 	int second, minute, hour, day, month, year;
-	DateTime(int second_, int minute_, int hour_, int day_, int month_, int year_);
-	int getCumMinute();
-	DateTime();
-	~DateTime();
-};	
+	DateTime() {};
+	DateTime(int second_, int minute_, int hour_) : second(second_), minute(minute_), hour(hour_), day(-1), month(-1), year(-1) {};
+	DateTime(int second_, int minute_, int hour_, int day_, int month_, int year_):second(second_), minute(minute_), hour(hour_), day(day_), month(month_), year(year_) {};
+	int getCumMinute()
+	{
+		return second / 60 + hour * 60 + minute;
+	}
+
+	DateTime operator+(DateTime time) 
+	{
+		DateTime res;
+		res.day = day; res.month = month; res.year = year;
+		res.second = (second + time.second)% 60;
+		int r_second = (second + time.second) / 60;
+		res.minute = (minute + time.minute + r_second) % 60;
+		int r_min = (minute + time.minute + r_second) / 60;
+		res.hour = hour + time.hour + r_min;
+		if (res.hour > 24) {
+			res.hour = 23;
+			res.second = 59;
+			res.minute = 59;
+		}
+		return res;
+	}
+
+	bool operator==(DateTime time) {
+		return (second == time.second && minute == time.minute && hour == time.hour);
+	}
+
+	bool operator>(DateTime time) {
+		if (hour > time.hour) 
+			return true;
+		if (minute > time.minute)
+			return true;
+		if (second > time.second)
+			return true;
+		return false;
+	}
+	
+	bool operator>=(DateTime time) {
+		if (*this == time || *this > time)
+			return true;
+		return false;
+	}
+
+	bool operator<(DateTime time) {
+		return !(*this >= time);
+	}
+
+	bool operator<=(DateTime time) {
+		return !(*this > time);
+	}
+
+
+
+};
