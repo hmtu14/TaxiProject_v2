441831e31eacf1d18ca981774f85ecb7b042be0b
PhungVanDuy <daicait23@gmail.com>
2017-05-19 11:08:05 +0700

    Fix ParkingPlace

--- TaxiProject_v2/ParkingPlace.cpp
+++ TaxiProject_v2/ParkingPlace.cpp
@@ -2,15 +2,15 @@
 
 
 ParkingPlace::ParkingPlace(vector<Coord> &lstCoord__) {
-	double xmin = lstCoord__[0].lat;
-	double ymin = lstCoord__[0].lng;
-	double xmax = lstCoord__[0].lat;
-	double ymax = lstCoord__[0].lng;
+	double xmin = lstCoord__[0].Latitude;
+	double ymin = lstCoord__[0].Longitude;
+	double xmax = lstCoord__[0].Latitude;
+	double ymax = lstCoord__[0].Longitude;
 	for (auto point : lstCoord__) {
-		if (point.lat < xmin) xmin = point.lat;
-		if (point.lat > xmax) xmax = point.lat;
-		if (point.lng < ymin) ymin = point.lng;
-		if (point.lng < ymax) ymax = point.lng;
+		if (point.Latitude < xmin) xmin = point.Latitude;
+		if (point.Latitude > xmax) xmax = point.Latitude;
+		if (point.Longitude < ymin) ymin = point.Longitude;
+		if (point.Longitude < ymax) ymax = point.Longitude;
 	}
 	topleft = Coord(xmin, ymax);
 	btmright = Coord(xmax, ymin);
@@ -22,18 +22,18 @@ ParkingPlace::ParkingPlace()
 
 Coord TopLeftCoord(vector<Coord> S)
 {
-	double lat_min = S[0].lat; 
+	double lat_min = S[0].Latitude; 
 	for (int i = 0; i < S.size(); i++)
-		lat_min = min(lat_min, S[i].lat); 
+		lat_min = min(lat_min, S[i].Latitude); 
 
 	double lng_min = 0; 
 	for (int i = 0; i < S.size(); i++)
-		if (S[i].lat == lat_min)
+		if (S[i].Latitude == lat_min)
 		{
-			if (lng_min == 0) lng_min = S[i].lng;
+			if (lng_min == 0) lng_min = S[i].Longitude;
 			else
-				if (S[i].lng < lng_min)
-					lng_min = S[i].lng;
+				if (S[i].Longitude < lng_min)
+					lng_min = S[i].Longitude;
 		}
 	
 	return Coord(lat_min, lng_min); 
@@ -42,18 +42,18 @@ Coord TopLeftCoord(vector<Coord> S)
 
 Coord BottomRightCoord(vector<Coord> S)
 {
-	double lat_max = S[0].lat;
+	double lat_max = S[0].Latitude;
 	for (int i = 0; i < S.size(); i++)
-		lat_max = max(lat_max, S[i].lat);
+		lat_max = max(lat_max, S[i].Latitude);
 
 	double lng_max = 0;
 	for (int i = 0; i < S.size(); i++)
-		if (S[i].lat == lat_max)
+		if (S[i].Latitude == lat_max)
 		{
-			if (lng_max == 0) lng_max = S[i].lng;
+			if (lng_max == 0) lng_max = S[i].Longitude;
 			else
-				if (S[i].lng > lng_max)
-					lng_max = S[i].lng;
+				if (S[i].Longitude > lng_max)
+					lng_max = S[i].Longitude;
 		}
 
 	return Coord(lat_max, lng_max);
@@ -61,15 +61,15 @@ Coord BottomRightCoord(vector<Coord> S)
 
 bool ParkingPlace::inParking(Coord x)
 {
-	//if (x.lat < this->topleft.lat || x.lng < this->topleft.lng)
+	//if (x.Latitude < this->topleft.Latitude || x.Longitude < this->topleft.Longitude)
 	//	return false; 
-	//if (x.lat > this->btmright.lat || x.lng > this->btmright.lng)
+	//if (x.Latitude > this->btmright.Latitude || x.Longitude > this->btmright.Longitude)
 	//	return false;
 	//Coord TopLeft = TopLeftCoord(this->lstCoord);
 	//Coord BottomRight = BottomRightCoord(this->lstCoord); 
-	//if (x.lat < TopLeft.lat || x.lng < TopLeft.lng)
+	//if (x.Latitude < TopLeft.Latitude || x.Longitude < TopLeft.Longitude)
 	//	return false;
-	//if (x.lat > BottomRight.lat || x.lng > BottomRight.lng)
+	//if (x.Latitude > BottomRight.Latitude || x.Longitude > BottomRight.Longitude)
 	//	return false;
 	return true; 
 }
